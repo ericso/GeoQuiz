ec6ffd817b82e759d3823ce42bdda7c3856c61c6
Eric So <me@eric.so>
2016-04-09 17:19:53 -0700

    Add animation to hide show answer button and reveal answer in CheatActivity.

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -1,11 +1,16 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -46,26 +51,49 @@ public class CheatActivity extends AppCompatActivity {
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
 
-        if (savedInstanceState != null) {
-            mIsCheater = savedInstanceState.getBoolean(CHEAT_STATUS, false);
-            if (mIsCheater) {
-                showAnswerView();
-                setAnswerShownResult(true);
-            }
-        }
-
         mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showAnswerView();
+                setAnswerTextView();
                 mIsCheater = true;
                 setAnswerShownResult(true);
+
+                // Fancy animation to hide the button and reveal the text
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator anim = ViewAnimationUtils.createCircularReveal(
+                            mShowAnswerButton, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
             }
         });
+
+        if (savedInstanceState != null) {
+            mIsCheater = savedInstanceState.getBoolean(CHEAT_STATUS, false);
+            if (mIsCheater) {
+                setAnswerTextView();
+                setAnswerShownResult(true);
+                mAnswerTextView.setVisibility(View.VISIBLE);
+                mShowAnswerButton.setVisibility(View.INVISIBLE);
+            }
+        }
     }
 
-    private void showAnswerView() {
+    private void setAnswerTextView() {
         if (mAnswerIsTrue) {
             mAnswerTextView.setText(R.string.true_button);
         } else {
